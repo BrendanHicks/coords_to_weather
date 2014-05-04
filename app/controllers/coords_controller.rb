3aa21c285e3f266f8362f003ebbdcb4fbada085d
BrendanHicks <Brendan-Hicks@kellogg.northwestern.edu>
2014-05-04 14:18:31 -0500

    Added Error Handling

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,8 +3,37 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 34.3373061
-    @longitude = -118.155289
+
+
+    if params[:location] == nil
+
+      @address = "Chicago, IL"
+
+    else
+
+          @address = params[:location]
+
+    end
+
+    @url_safe_address = URI.encode(@address)
+            full_url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=true"
+
+        raw_data = open(full_url).read
+            parsed_data = JSON.parse(raw_data)
+                results = parsed_data["results"]
+
+                    if results[0] != nil
+
+                        @longitude   = results[0]["geometry"]["location"]['lng']
+                        @latitude    = results[0]["geometry"]["location"]["lat"]
+
+                    else
+
+                        @longitude =-87.6297982
+                        @latitude = 41.8781136
+
+                    end
+
     your_api_key = "c59ddc888642d079061fe241c99d7145"
 
         @url = "https://api.forecast.io/forecast/#{your_api_key}/#{@latitude},#{@longitude}"
